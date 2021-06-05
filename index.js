3dd9bf05eb67068ece5dadc09d8f094e5a84e706
BalaRohanKotni <balarohan.k@gmail.com>
2021-06-05 17:35:07 +0530

    hello text animation

---
+++ index.js
@@ -0,0 +1,17 @@
+const helloline = document.getElementById("helloline");
+
+let pathLength = helloline.getTotalLength();
+// console.log(pathLength)
+helloline.setAttribute("stroke-dasharray", pathLength);
+helloline.setAttribute("stroke-dashoffset", pathLength);
+console.log(helloline.innerHTML)
+helloline.innerHTML = `<animate attributeName="stroke-dashoffset" begin='0s' dur="3s" to="0" fill="freeze" />`;
+
+// var vara = new Vara("#helloline", "PacificoSLO.json", [{
+//     text: "hello",
+//     fontSize: 48,
+//     duration: 1000,
+// },], {
+//     strokeWidth: 2,
+//     textAlign: "center"
+// });
